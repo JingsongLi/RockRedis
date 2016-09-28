47f62e9bbbbb6f6dd658530118bdac0ac27df0f7
JingsongLi <lzljs3620320@aliyun.com>
2016-09-28 19:51:18 +0800

    first

---
+++ src/main/java/Main.java
@@ -0,0 +1,75 @@
+import org.rocksdb.*;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+public class Main {
+
+    private static Random rand_ = new Random(1000);
+
+    public static void main(String[] args) throws RocksDBException, IOException, InterruptedException {
+        String file = "/Users/jslee/Downloads/tmp/";
+        deleteDir(new File(file));
+        Options options = new Options();
+        options.setCreateIfMissing(true);
+//        options.setCompactionStyle(CompactionStyle.UNIVERSAL);
+        RocksDB db_ = RocksDB.open(options, file);
+        RandomGenerator gen_ = new RandomGenerator(0, 0.5d);
+        WriteOptions writeOpt = new WriteOptions();
+        writeOpt.setDisableWAL(true);
+        int keySize_ = 100;
+        int valueSize_ = 10000;
+        byte[] key = new byte[keySize_];
+        byte[] value = new byte[valueSize_];
+        long startTime_ = System.nanoTime();
+        long bytes = 0;
+        long keyRange_ = 1000000;
+        for (int i = 0; i < keyRange_; i++) {
+            getRandomKey(key, keyRange_);
+            gen_.generate(value);
+            db_.put(writeOpt, key, value);
+            bytes += keySize_;
+            bytes += valueSize_;
+        }
+        long endTime = System.nanoTime();
+        double elapsedSeconds =
+                1.0d * (endTime - startTime_) / TimeUnit.SECONDS.toNanos(1);
+        System.out.println(String.valueOf(bytes / 1048576.0 / elapsedSeconds) + "MB/s");
+        System.out.println("use time: " + elapsedSeconds + " s");
+        db_.close();
+
+        Thread.sleep(1000*60);
+    }
+
+    private static void getRandomKey(byte[] key, long range) {
+        generateKeyFromLong(key, Math.abs(rand_.nextLong() % range));
+    }
+
+    private static void generateKeyFromLong(byte[] slice, long n) {
+        assert(n >= 0);
+        int startPos = 0;
+
+        for (int i = slice.length - 1; i >= startPos; --i) {
+            slice[i] = (byte) ('0' + (n % 10));
+            n /= 10;
+        }
+    }
+
+    private static boolean deleteDir(File dir) {
+        if (dir.isDirectory()) {
+            String[] children = dir.list();
+            if (children != null) {
+                for (String aChildren : children) {
+                    boolean success = deleteDir(new File(dir, aChildren));
+                    if (!success) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return dir.delete();
+    }
+
+}
---
+++ src/main/java/RandomGenerator.java
@@ -0,0 +1,56 @@
+import java.util.Random;
+
+public class RandomGenerator {
+    private final byte[] data_;
+    private int dataLength_;
+    private int position_;
+    private double compressionRatio_;
+    Random rand_;
+
+    public RandomGenerator(long seed, double compressionRatio) {
+      // We use a limited amount of data over and over again and ensure
+      // that it is larger than the compression window (32KB), and also
+      byte[] value = new byte[100];
+      // large enough to serve all typical value sizes we want to write.
+      rand_ = new Random(seed);
+      dataLength_ = value.length * 10000;
+      data_ = new byte[dataLength_];
+      compressionRatio_ = compressionRatio;
+      int pos = 0;
+      while (pos < dataLength_) {
+        compressibleBytes(value);
+        System.arraycopy(value, 0, data_, pos,
+                         Math.min(value.length, dataLength_ - pos));
+        pos += value.length;
+      }
+    }
+
+    private void compressibleBytes(byte[] value) {
+      int baseLength = value.length;
+      if (compressionRatio_ < 1.0d) {
+        baseLength = (int) (compressionRatio_ * value.length + 0.5);
+      }
+      if (baseLength <= 0) {
+        baseLength = 1;
+      }
+      int pos;
+      for (pos = 0; pos < baseLength; ++pos) {
+        value[pos] = (byte) (' ' + rand_.nextInt(95));  // ' ' .. '~'
+      }
+      while (pos < value.length) {
+        System.arraycopy(value, 0, value, pos,
+                         Math.min(baseLength, value.length - pos));
+        pos += baseLength;
+      }
+    }
+
+    public void generate(byte[] value) {
+      if (position_ + value.length > data_.length) {
+        position_ = 0;
+        assert(value.length <= data_.length);
+      }
+      position_ += value.length;
+      System.arraycopy(data_, position_ - value.length,
+                       value, 0, value.length);
+    }
+  }
